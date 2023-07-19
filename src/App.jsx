76790e9e41cacb0e30580b202ab0a3ec505d48ee
Ivan Silva <silvaivan280@gmail.com>
2023-07-19 11:18:19 -0300

    agregue el componente AdministradorUsuarios de se lista los usuarios

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
-import AdministradorProductos from './components/views/Administrador/AdministradorProductos';
+import AdministradorPedidios from './components/views/Administrador/AdministradorPedidos';
+// import AdministradorUsuarios from './components/views/Administrador/AdministradorUsuarios';
+// import AdministradorProductos from './components/views/Administrador/AdministradorProductos';
 
 function App() {
   
 
   return (
     <>
-      <AdministradorProductos></AdministradorProductos>
+      {/* <AdministradorProductos></AdministradorProductos> */}
+      {/* <AdministradorUsuarios></AdministradorUsuarios> */}
+      <AdministradorPedidios></AdministradorPedidios>
     </>
   )
 }
