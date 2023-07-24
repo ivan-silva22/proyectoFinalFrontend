900310bb6e7f2be22c3392242b5ce9fd18db9f5b
Ivan Silva <silvaivan280@gmail.com>
2023-07-24 18:41:24 -0300

    agregue el componente EditarProducto

--- src/components/views/Producto/EditarProducto.jsx
+++ src/components/views/Producto/EditarProducto.jsx
@@ -1,19 +1,17 @@
 import { Form, Button } from "react-bootstrap";
 
-const CrearProducto = () => {
-
-    
-
+const EditarProducto = () => {
     return (
         <section className="container mainSection">
-      <h1 className="display-4 mt-5">Nuevo producto</h1>
+      <h1 className="display-4 mt-5">Editar producto</h1>
       <hr />
-      <Form>
+      <Form >
         <Form.Group className="mb-3" controlId="formNombreProducto">
           <Form.Label>Producto*</Form.Label>
           <Form.Control
             type="text"
             placeholder="Ej: Lasagna"
+            
           />
           <Form.Text className="text-danger">
             
@@ -24,7 +22,7 @@ const CrearProducto = () => {
           <Form.Control
             type="number"
             placeholder="Ej: 1000"
-        
+            
           />
           <Form.Text className="text-danger">
             
@@ -35,7 +33,7 @@ const CrearProducto = () => {
           <Form.Control
             type="text"
             placeholder="Ej: Lasagna rellena de jamón..."
-            
+           
           />
           <Form.Text className="text-danger">
             
@@ -55,7 +53,7 @@ const CrearProducto = () => {
         <Form.Group className="mb-3" controlId="formPrecio">
           <Form.Label>Categoria*</Form.Label>
           <Form.Select
-           
+            
           >
             <option value="">Seleccione una opcion</option>
             <option value="pastas">Pastas</option>
@@ -63,6 +61,7 @@ const CrearProducto = () => {
             <option value="bebidas">Bebidas</option>
           </Form.Select>
           <Form.Text className="text-danger">
+            
             
           </Form.Text>
         </Form.Group>
@@ -74,4 +73,4 @@ const CrearProducto = () => {
     );
 };
 
-export default CrearProducto;
+export default EditarProducto;
