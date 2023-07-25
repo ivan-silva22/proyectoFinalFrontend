4dac11632263c23533fe3688489e801331d66d4f
Ivan Silva <silvaivan280@gmail.com>
2023-07-25 20:27:07 -0300

    agregue la funcion consultarAgregarProducto donde hace la peticion POST

--- src/components/helpers/queries.js
+++ src/components/helpers/queries.js
@@ -1,4 +1,21 @@
+
+
 const URLProducto = import.meta.env.VITE_API_PRODUCTO;
+
+export const consultarAgregarProducto = async (producto) =>{
+    try {
+        const respuesta = await fetch(URLProducto, {
+            method: "POST",
+            headres: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(producto)
+        });
+        return respuesta;
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 export const consultarListaProductos = async () =>{
     try {
