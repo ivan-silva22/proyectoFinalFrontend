5066879d0f8e8193662a65d67f3d241430907369
Ivan Silva <silvaivan280@gmail.com>
2023-07-25 10:34:46 -0300

    agregue el componente ItemProducto

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+// import EditarProducto from './components/views/Producto/EditarProducto';
 // import Pedidos from './components/views/Pedidos';
 // import AdministradorPedidios from './components/views/Administrador/AdministradorPedidos';
 // import AdministradorUsuarios from './components/views/Administrador/AdministradorUsuarios';
-// import AdministradorProductos from './components/views/Administrador/AdministradorProductos';
+import AdministradorProductos from './components/views/Administrador/AdministradorProductos';
+// import CrearProducto from './components/views/Producto/CrearProducto';
 
 function App() {
   
 
   return (
     <>
-      {/* <AdministradorProductos></AdministradorProductos> */}
+      <AdministradorProductos></AdministradorProductos>
       {/* <AdministradorUsuarios></AdministradorUsuarios> */}
       {/* <AdministradorPedidios></AdministradorPedidios> */}
       {/* <Pedidos></Pedidos> */}
+      {/* <CrearProducto></CrearProducto> */}
+      {/* <EditarProducto></EditarProducto> */}
     </>
   )
 }
--- src/components/views/Producto/ItemProducto.jsx
+++ src/components/views/Producto/ItemProducto.jsx
@@ -1,21 +1,26 @@
 import { Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const ItemProducto = () => {
     return (
         <tr>
         {/* <td>{props.producto._id}</td> */}
-        <td>{producto._id}</td>
-        <td>{producto.nombreProducto}</td>
-        <td>${producto.precio}</td>
-        <td>{producto.imagen}</td>
-        <td>{producto.categoria}</td>
-        <td>
-          <Link className="btn btn-warning" to={'/administrador/editar-producto/'+producto._id}>Editar</Link>
-          <Button variant="danger" >
-            Borrar
-          </Button>
-        </td>
+        <td>1</td>
+                  <td>Empanadas de carne</td>
+                  <td>
+                    alguna descripcion
+                  </td>
+                  <td>
+                    https://images.pexels.com/photos/8280002/pexels-photo-8280002.jpeg
+                  </td>
+                  <td>$2500</td>
+                  <td>Entrada</td>
+                  <td>
+                    <div className="d-flex justify-content-around">
+                      <Button variant="warning">Editar</Button>
+                      <Button variant="danger">Eliminar</Button>
+                    </div>
+                  </td>
       </tr>
     );
 };
