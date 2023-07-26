13db258ad7ad254c34d75ef37ad78f1c4b54f5e9
Huguito1998 <huguitotriunfetti@hotmail.com>
2023-07-26 19:19:58 -0300

    maquetado registro
    realizacion del maquetado registro

--- src/components/views/Registro.jsx
+++ src/components/views/Registro.jsx
@@ -3,27 +3,47 @@ import Form from 'react-bootstrap/Form';
 
 function Registro() {
   return (
-    <Form>
+    <Form className='container m-5 p-5 letraDancing fs-5'>
+
+<Form.Group className="mb-3" controlId="formNombre">
+        <Form.Label>Nombre</Form.Label>
+        <Form.Control type="nombre" placeholder="Ingresar nombre" />
+        <Form.Text className="text-muted">
+        
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formApellido">
+        <Form.Label>Apellido</Form.Label>
+        <Form.Control type="Apellido" placeholder="Ingresar apellido" />
+        <Form.Text className="text-muted">
+        </Form.Text>
+      </Form.Group>
+
+      
+        
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
+        <Form.Label>Email</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
         <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
+        Nunca compartiremos su correo electrónico con nadie más.
         </Form.Text>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
+        <Form.Label>Contraseña</Form.Label>
         <Form.Control type="password" placeholder="Password" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
+
+      
       <Button variant="primary" type="submit">
-        Submit
+        Enviar
       </Button>
     </Form>
   );
 }
 
 export default Registro;
+
+      
+
