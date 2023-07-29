3be7bc6d47f706b27f3016d26460f4d2391ee218
Tomas Quispe <tomasbquispe@gmail.com>
2023-07-29 18:31:27 -0300

    termine pag 404

--- src/components/views/error404.jsx
+++ src/components/views/error404.jsx
@@ -1,13 +1,15 @@
 import "../../css/error404.css";
+import "../../css/App.css";
+
 import error from '../../assets/error404.jpg'
 const Error404 = () => {
     return (
-        <div className="container-fluid">
+      <div className="container-fluid fondoBlanco letraGris">
         <div className="row justify-content-center d-flex">
           <div className="error">
             <img src="../../assets/error404.jpg" alt="error404" />
-            <p>Lo siento, la página que buscas no se encuentra disponible.</p>
-            <p className="icons">
+            <p className="letraAmatic">Lo siento, la página que buscas no se encuentra disponible.</p>
+            <p className="icons letraAmatic">
               <a href="/">Haz clic aquí para volver a la página principal</a>
             </p>
           </div>
