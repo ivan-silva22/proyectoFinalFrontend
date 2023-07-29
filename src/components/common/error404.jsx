bd47ed4e0ab7bbe34d94792823949d49878a490e
Tomas Quispe <tomasbquispe@gmail.com>
2023-07-29 18:01:32 -0300

    resolvi algunos errores de la pag 404

--- src/components/common/error404.jsx
+++ src/components/common/error404.jsx
@@ -1,14 +1,18 @@
-import { Button } from 'react-bootstrap';
-import error from '../../assets/fondo-Error404'
+import "../../css/error404.css";
+import error from '../../assets/error404.jpg'
 const Error404 = () => {
     return (
-        <section className="mainSection text-center">
-            <img src={error} alt="fondo pagina error 404" />
-            <div>
-            <Button variant='primary' >Volver al inicio</Button>
-
-            </div>
-        </section>
+        <div className="container-fluid">
+        <div className="row justify-content-center d-flex">
+          <div className="error">
+            <img src="../../assets/error404.jpg" alt="error404" />
+            <p>Lo siento, la página que buscas no se encuentra disponible.</p>
+            <p className="icons">
+              <a href="/">Haz clic aquí para volver a la página principal</a>
+            </p>
+          </div>
+        </div>
+      </div>
     );
 };
 
